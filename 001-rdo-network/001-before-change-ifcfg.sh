b2e3d05e85c1a5b5a214dd40d187f7828abb4e02
Haoran Feng <haoran.feng@alo7.com>
2017-07-21 14:22:12 +0800

    new update

--- 001-rdo-network/001-before-change-ifcfg.sh
+++ 001-rdo-network/001-before-change-ifcfg.sh
@@ -1,1 +1,11 @@
-packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ovs-bridge-interfaces=br-ex:enp0s8 --os-neutron-ml2-type-drivers=vxlan,flat
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl enable network
+systemctl start network
+yum install -y git tmux
+yum install -y centos-release-openstack-ocata
+yum install -y openstack-packstack
+
+packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ovs-bridge-interfaces=br-ex:em1 --os-neutron-ml2-type-drivers=vxlan,flat
